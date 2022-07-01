1729173dd8fdc8589cd3c44c0721a4a7499bb130
GHeeJeon <jhjeon_99@naver.com>
2022-07-01 19:02:26 +0900

    update Combine the two Tweens

--- route_transition/lib/main.dart
+++ route_transition/lib/main.dart
@@ -33,16 +33,12 @@ Route _createRoute() {
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
 	const begin = Offset(0.0, 1.0);
   	const end = Offset.zero;
+	var curve = Curves.easeInOutQuint;
 
-	var curve = Curves.ease;
-	var curveTween = CurveTween(curve: curve);
-
-  	final tween = Tween(begin: begin, end: end);
-
-  	final offsetAnimation = animation.drive(tween);
+	var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
 
 	return SlideTransition(
-	  position: offsetAnimation,
+	  position: animation.drive(tween),
 	  child: child,
 	  );
 	},
