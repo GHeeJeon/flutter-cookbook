791ac6d98496dfdf8831aed630e20ef85365de1c
GHeeJeon <jhjeon_99@naver.com>
2022-07-01 19:41:42 +0900

    create physics_simulation

---
+++ physics_simulation/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+	const MaterialApp(
+	  //debugShowCheckedModeBanner: false,
+	  home: PhysicsCardDragDemo()
+	)
+  );
+}
+
+class PhysicsCardDragDemo extends StatelessWidget {
+  const PhysicsCardDragDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const DraggableCard(
+        child: FlutterLogo(
+          size: 128,
+        ),
+      ),
+    );
+  }
+}
+
+class DraggableCard extends StatefulWidget {
+  const DraggableCard({required this.child, super.key});
+
+  final Widget child;
+
+  @override
+  _DraggableCardState createState() => _DraggableCardState();
+}
+
+class _DraggableCardState extends State<DraggableCard> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      child: Card(
+        child: widget.child,
+      ),
+    );
+  }
+}
